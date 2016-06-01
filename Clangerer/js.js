bfa8ffb748826132d6a83eb35eb8e6eaefaf29e5
Jeff Wright <jeffpwright@verizon.net>
2016-06-01 11:13:40 -0500

    Add files via upload

--- Clangerer/js.js
+++ Clangerer/js.js
@@ -1,1 +1,6 @@
-﻿
+﻿function moveIt() {
+    var coords = new Array(10, 50, 100, 130, 175, 225, 260, 300, 320, 350);
+    var x = coords[Math.floor((Math.random() * 10))];
+    var y = coords[Math.floor((Math.random() * 10))];
+    $("#elusiveText").css({ "top": y + "px", "left": x + "px" });
+}

0a299cf57d4a3c36d7fbdee0d6e24517da59afea
Jeff Wright <jeffpwright@hotmail.com>
2016-06-20 15:58:05 -0500

    eventstriggeringevents

--- BasicJsJq/js/events.js
+++ BasicJsJq/js/events.js
@@ -24,6 +24,22 @@ function noWrapper(e)
 
 function onloadHandler()
 {
-	document.getElementById("div1").addEventListener('click', yesWrapper, false);
-	document.getElementById("div2").addEventListener('click', noWrapper, false);
+	var employee = document.getElementById("Employee");
+	employee.addEventListener('click', simpleClick, false);
+	var registered = document.getElementById("Registered");
+	registered.addEventListener('click', eventClick, false);
+}
+
+function simpleClick(e)
+{
+	var cb = document.getElementById("check" + e.target.id);
+	cb.click();
+}
+
+function eventClick(e)
+{
+	var event = document.createEvent("MouseEvents");
+	event.initMouseEvent("click", true, true, window, 0,0,0,0,0, false, false, false, false, 0, null);
+	var cb = document.getElementById("check" + e.target.id);
+	cb.dispatchEvent(event);
 }
